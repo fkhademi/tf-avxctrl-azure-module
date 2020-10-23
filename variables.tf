ac09f3c2420da2cb99ddca40d182bf9e5650eaf2
Frey Khademi <frey.khademi@gmail.com>
2020-10-23 09:32:47 +0200

    fix var

--- variables.tf
+++ variables.tf
@@ -9,3 +9,4 @@ variable "name" { default = "avx-shared" }
 variable "vnet_cidr" { default = "10.0.0.0/20" }
 variable "vnet_subnet_cidr" { default = "10.0.0.0/24" }
 variable "os_pw" { default = "Password1234!" }
+variable "agreement" { default = true }
